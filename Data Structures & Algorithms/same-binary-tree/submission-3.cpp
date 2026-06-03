27aba2c47a2309ff3e262ad8c8f6a4dd0e329e75
Neha Prabhu <nehaprabhup17@gmail.com>
2026-06-03 12:09:25 +0530

    Add: same-binary-tree - submission-3

---
+++ Data Structures & Algorithms/same-binary-tree/submission-3.cpp
@@ -0,0 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (!p && !q)
+        {
+            return true;
+        }
+        
+        if (p && q && p->val != q->val)
+        {
+            return false;
+        }
+        
+        if (!p && q || p && !q)
+        {
+            return false;
+        }
+
+        if (!p->left && !p->right && !q->left && !q->right)
+        {
+            return true;
+        }
+
+        if (p->left && q->left && p->left->val != q->left->val)
+        {
+            return false;
+        }
+
+        if (p->right && q->right && p->right->val != q->right->val)
+        {
+            return false;
+        }
+
+        if (p->left && !q->left || !p->left && q->left)
+        {
+            return false;
+        }
+
+        if (p->right && !q->right || !p->right && q->right)
+        {
+            return false;
+        }
+
+        return isSameTree(p->right, q->right) && isSameTree(p->left, q->left);
+    }
+};
