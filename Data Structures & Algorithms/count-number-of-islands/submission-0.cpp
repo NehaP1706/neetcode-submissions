f8e9f2d655f4519bc8aa6f4a0746be314f328e70
Neha Prabhu <nehaprabhup17@gmail.com>
2026-06-05 09:26:06 +0530

    Add: count-number-of-islands - submission-0

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-0.cpp
@@ -0,0 +1,83 @@
+class DSU {
+public:
+    vector<int> parent, sz;
+
+    DSU(int n) {
+        parent.resize(n);
+        sz.resize(n, 1);
+
+        for (int i = 0; i < n; i++)
+            parent[i] = i;
+    }
+
+    int find(int x) {
+        if (parent[x] == x)
+            return x;
+
+        return parent[x] = find(parent[x]);
+    }
+
+    void unite(int a, int b) {
+        a = find(a);
+        b = find(b);
+
+        if (a == b)
+            return;
+
+        if (sz[a] < sz[b])
+            swap(a, b);
+
+        parent[b] = a;
+        sz[a] += sz[b];
+    }
+};
+
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+
+        DSU dsu(m * n);
+
+        vector<int> dx = {1, 0};
+        vector<int> dy = {0, 1};
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+
+                if (grid[i][j] == '0')
+                    continue;
+
+                int u = i * n + j;
+
+                for (int k = 0; k < 2; k++) {
+                    int ni = i + dx[k];
+                    int nj = j + dy[k];
+
+                    if (ni < m && nj < n &&
+                        grid[ni][nj] == '1')
+                    {
+                        int v = ni * n + nj;
+                        dsu.unite(u, v);
+                    }
+                }
+            }
+        }
+
+        unordered_set<int> islands;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+
+                if (grid[i][j] == '1') {
+                    islands.insert(
+                        dsu.find(i * n + j)
+                    );
+                }
+            }
+        }
+
+        return islands.size();
+    }
+};
