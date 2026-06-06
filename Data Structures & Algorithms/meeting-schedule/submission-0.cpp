07ea3f7b21a837aea49aace7fedc38a07d8cefb6
Neha Prabhu <nehaprabhup17@gmail.com>
2026-06-07 00:35:33 +0530

    Add: meeting-schedule - submission-0

---
+++ Data Structures & Algorithms/meeting-schedule/submission-0.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    bool canAttendMeetings(vector<Interval>& intervals) {
+        int n = intervals.size();
+        vector<pair<int, int>> ps;
+
+        for (int i=0; i<n; i++)
+        {
+            ps.push_back({intervals[i].start, 1});
+            ps.push_back({intervals[i].end, -1});
+        }
+
+        sort(ps.begin(), ps.end());
+        int count = 0;
+
+        for (int i=0; i<2*n; i++)
+        {
+            count += ps[i].second;
+
+            if (count > 1)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
