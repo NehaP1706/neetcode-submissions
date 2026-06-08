fd330e42756b5da0ab75078c0058f0e100299c0a
Neha Prabhu <nehaprabhup17@gmail.com>
2026-06-08 15:33:56 +0530

    Add: coin-change-ii - submission-0

---
+++ Data Structures & Algorithms/coin-change-ii/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        vector<int> dp(amount + 1, 0);
+
+        dp[0] = 1;
+
+        for (int c : coins) {
+            for (int x = c; x <= amount; x++) {
+                dp[x] += dp[x - c];
+            }
+        }
+
+        return dp[amount];
+    }
+};
